6d48d5158510dd48b48c0efd59b378f52a5b6e8d
SmartDude04 <aidan.j.oconnor@outlook.com>
2023-10-01 21:07:24 -0400

    Working on equations project

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -36,3 +36,33 @@ function fNet() {
     document.getElementById("fnet-answer").value = answer;
 
 }
+
+function eHv() {
+    // Pull values
+    const h = 6.6260e-34
+    const v = document.getElementById("ehv-v").value;
+
+    // Calculate energy
+    let E = h * v;
+    let numDigits = getLength(E) - 1;
+    const divisor = 1000;
+
+    // Concert to exponential && round number
+    document.getElementById("ehv-answer").value = E;
+}
+
+function aT2() {
+    let x = document.getElementById("at2-x").value;
+    x = parseInt(x, 10);
+    let v = document.getElementById("at2-v").value;
+    v = parseInt(v, 10);
+    let a = document.getElementById("at2-a").value;
+    a = parseInt(a, 10);
+    let t = document.getElementById("at2-t").value;
+    t = parseInt(t, 10);
+
+    let answer = (0.5 * a * (t ** 2)) + (v * t) + (x);
+    document.getElementById("at2-answer").value = answer;
+}
+
+Math.integral
