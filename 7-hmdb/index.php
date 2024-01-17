281851aa8345ebd38f5a0ab06c035c0a13321c01
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-01-17 14:10:52 -0500

    Changed name

--- 7-hmdb/index.php
+++ 7-hmdb/index.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>hMDB - The Hanover Movie Database</title>
+        <title>HMDb - The Hanover Movie Database</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     </head>
 
     <body>
         <div class="container">
-            <h1>hMDB: The <em>Hanover</em> Movie Database</h1>
+            <h1>HMDb: The <em>Hanover</em> Movie Database</h1>
 <?php
 
 include('library.php');
