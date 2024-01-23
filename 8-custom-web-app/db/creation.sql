ed78220b0a165da0620bcc9ee233a8354b3c4c32
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-01-23 11:24:41 -0500

    Jan 23 in-class work

---
+++ 8-custom-web-app/db/creation.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `tpm`.`passwords` (
+  `pwd_id` INT NOT NULL AUTO_INCREMENT,
+  `pwd_title` VARCHAR(45) NOT NULL,
+  `pwd_username` VARCHAR(45) NULL,
+  `pwd_email` VARCHAR(45) NULL,
+  `pwd_password` VARCHAR(45) NOT NULL,
+  `pwd_url` VARCHAR(100) NOT NULL,
+  `pwd_totp` VARCHAR(45) NULL,
+  `pwd_notes` VARCHAR(512) NULL,
+  PRIMARY KEY (`pwd_id`),
+  UNIQUE INDEX `pwd_id_UNIQUE` (`pwd_id` ASC) VISIBLE);
+
+CREATE TABLE `tpm`.`security_questions` (
+  `sec_id` INT NOT NULL AUTO_INCREMENT,
+  `sec_pwd_id` INT NOT NULL,
+  `sec_title` VARCHAR(45) NOT NULL,
+  `sec_answer` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`sec_id`),
+  UNIQUE INDEX `sec_id_UNIQUE` (`sec_id` ASC));
--- 8-custom-web-app/web/index.php
+++ 8-custom-web-app/web/index.php
@@ -1,1 +1,21 @@
-1
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1080, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>TPM • Terrible Password Manager</title>
+</head>
+<body>
+
+    <nav class="grid-container" id="navbar">
+        <!-- <a href="index.php" class="element">
+            <img src="img/logo.png" alt="logo">
+        </a> -->
+        <a class="element" href="index.php">Vault</a>
+        <a class="element" href="index.php?loc=generator">Password Generator</a>
+        <a class="element" href="index.php?loc=creation">New Password</a>        
+    </nav>
+
+</body>
+</html>
