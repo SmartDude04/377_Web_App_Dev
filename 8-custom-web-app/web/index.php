e3047460caf52911bb3356b86b3c9ed338ed08b9
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-01-24 11:24:27 -0500

    Classwork 1/24 - Working on box for nav items

--- 8-custom-web-app/web/index.php
+++ 8-custom-web-app/web/index.php
@@ -3,19 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1080, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo rand()?>"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <title>TPM • Terrible Password Manager</title>
 </head>
 <body>
-
+    <a href="index.php">
+        <img src="img/logo.png" alt="logo link" class="nav-logo">
+    </a>
     <nav class="grid-container" id="navbar">
-        <!-- <a href="index.php" class="element">
-            <img src="img/logo.png" alt="logo">
-        </a> -->
-        <a class="element" href="index.php">Vault</a>
-        <a class="element" href="index.php?loc=generator">Password Generator</a>
-        <a class="element" href="index.php?loc=creation">New Password</a>        
+        <a class="element nav-item" href="index.php">Vault</a>
+        <a class="element nav-item" href="index.php?loc=generator">Password Generator</a>
+        <a class="element nav-item" href="index.php?loc=creation">New Password</a>
     </nav>
+
+    
 
 </body>
 </html>
