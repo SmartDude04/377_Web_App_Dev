18043a40923dab9a668ad0c1ed1eddc9e1eb056f
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-01-25 11:25:01 -0500

    1/25 Classwork

---
+++ 8-custom-web-app/web/homepage.php
@@ -0,0 +1,34 @@
+
+<!-- Main heading and description -->
+<div class="heading">
+    <img src="img/homepage-heading.png" alt="TPM - A Terrible Password Manager" id="homepage-heading">
+</div>
+
+<div class="desc">
+    <h1 id="desc-header">The only password manager you can trust.</h1>
+    <p>
+        In the modern age where quantum computing will soon break most encryption, why want a password manager that relies on frankly
+        old tech? TPM is the first password manager to revolutionize the structure of a password manager. Say goodbye to logging in - 
+        with TPM all your passwords are always accessible to the end user. And, with passwords being unencrypted in the database, 
+        you can rest easy knowing you can access your login info even if you don't feel like going to the website.
+    </p>
+</div>
+
+<img src="img/wave-transition.svg" alt="" class="transition">
+
+<div class="get-started">
+    <h1 id="started-title">Get Started</h1>
+    <div id="get-started-container">
+        <h1 class="started-text">Access Your Vault: </h1>
+        <button class="started-button" href="https://google.com">My Vault</button>
+        <h1 class="started-text">Explore the password generator: </h1>
+        <button class="started-button">Generator</button>
+        <h1 class="started-text">Create new passwords:  </h1>
+        <button class="started-button">New</button>
+    </div>
+</div>
+
+<?php
+
+$conn = dbConnect();
+
--- 8-custom-web-app/web/index.php
+++ 8-custom-web-app/web/index.php
@@ -3,23 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=1080, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <link rel="stylesheet" type="text/css" href="css/styles.css?v=<?php echo rand()?>"/>
-<?php
+    <?php
 
-// Get proper css for each page
-if (!isset($_GET["loc"]))
-{
-    echo '<link rel="stylesheet" type="text/css" href="css/panel.css?v=' . rand() . '"/>';
-}
-elseif($_GET["loc"] == "generator")
-{
-    echo '<link rel="stylesheet" type="text/css" href="css/generator.css?v=' . rand() . '"/>';}
-elseif($_GET["loc"] == "creation")
-{
-    echo '<link rel="stylesheet" type="text/css" href="css/creation.css?v=' . rand() . '"/>';
-}
+    // Get proper css for each page
+    if (!isset($_GET["loc"]))
+    {
+        echo '<link rel="stylesheet" type="text/css" href="css/homepage.css?v=' . rand() . '"/>';
+    }
+    elseif($_GET["loc"] == "panel")
+    {
+        echo '<link rel="stylesheet" type="text/css" href="css/panel.css?v=' . rand() . '"/>';
+    }
+    elseif($_GET["loc"] == "generator")
+    {
+        echo '<link rel="stylesheet" type="text/css" href="css/generator.css?v=' . rand() . '"/>';
+    }
+    elseif($_GET["loc"] == "creation")
+    {
+        echo '<link rel="stylesheet" type="text/css" href="css/creation.css?v=' . rand() . '"/>';
+    }
 
-?>
+    ?>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <title>TPM • Terrible Password Manager</title>
 </head>
@@ -28,30 +34,34 @@ elseif($_GET["loc"] == "creation")
         <img src="img/logo.png" alt="logo link" class="nav-logo">
     </a>
     <nav class="grid-container grid-fit" id="navbar">
-        <a class="element nav-item" href="index.php">My Vault</a>
-        <a class="element nav-item" href="index.php?loc=generator">Password Generator</a>
-        <a class="element nav-item" href="index.php?loc=creation" id="new-password-box">New Password</a>
+        <a class="nav-item" href="index.php?loc=panel">My Vault</a>
+        <a class="nav-item" href="index.php?loc=generator">Password Generator</a>
+        <a class="nav-item" href="index.php?loc=creation" id="new-password-box">New Password</a>
     </nav>
 
     <div class="content">
-<?php
+    <?php
 
-require "includes/dbc.inc.php";
+    require "includes/dbc.inc.php";
 
-if (!isset($_GET["loc"]))
-{
-    require "panel.php";
-}
-elseif($_GET["loc"] == "generator")
-{
-    require "generator.php";
-}
-elseif($_GET["loc"] == "creation")
-{
-    require "creation.php";
-}
+    if (!isset($_GET["loc"]))
+    {
+        include "homepage.php";
+    }
+    elseif($_GET["loc"] == "panel")
+    {
+        include "panel.php";
+    }
+    elseif($_GET["loc"] == "generator")
+    {
+        include "generator.php";
+    }
+    elseif($_GET["loc"] == "creation")
+    {
+        include "creation.php";
+    }
 
-?>
+    ?>
     </div>
 
 </body>
